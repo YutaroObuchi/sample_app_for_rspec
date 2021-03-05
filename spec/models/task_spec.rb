5479aa8add198bba5515acd2ebf6fc89d0bc198c
YutaroObuchi <buchiyu-10.one@docomo.ne.jp>
2021-03-06 01:15:02 +0900

    [Modify] マッチャを使用し、可読性をあげた

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,23 +3,36 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'バリデーション' do
 
-  	it 'titleが存在する' do 
-      task = FactoryBot.build(:task, title: nil)
-      task.valid?
-      expect(task.errors[:title]).to include("can't be blank")
+  	it '全ての属性が有効である' do
+  	  task = build(:task)
+  	  expect(task).to be_valid
+  	  except(task.errors).to be_empty
   	end
 
-  	it 'statusが存在する' do
-      task = FactoryBot.build(:task, status: nil)
-      task.valid?
-      expect(task.errors[:status]).to include("can't be blank")
+  	it 'タイトルなしでは無効である' do 
+  	  task_without_title = build(:task, title: "")
+      expect(task_without_title).to be_invalid
+      expect(task_without_title.eroors).to eq ["can't be blank"]
   	end
 
-  	it 'titleが一意性を持っている' do
-  	  FactoryBot.create(:task, title: "mtg準備")
-      task = FactoryBot.build(:task, title: "mtg準備")
-      task.valid?
-      expect(task.errors[:title]).to include("has already been taken")
-  	end  	  		
+  	it 'statusなしでは無効である' do
+      task_without_status = build(:task, status: "")
+      except(task_without_status).to be_invalid
+      except(task.errors).to eq ["can't be blank"]
+  	end
+
+  	it '重複したタイトルでは無効である' do
+  	  task = create(:task)
+      task_title_has_unique = build(:task, title: task.title)
+      expect(task_title_has_unique).to be_invalid
+      expect(task.eroors[:title]).to eq ["has already been taken"]
+    end  
+
+    it '別のタイトルであれば有効である' do
+      task = create(:task)
+      task_title_another = build(:task, title: "別のタイトル")
+      except(task_title_another).to be_valid
+      except(task_title_another.eroors).to be_empty
+    end
   end
 end
